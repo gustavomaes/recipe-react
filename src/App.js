f71e526451d8e8f9cb6a0011adb6b213ffd2d66b
Gustavo Maes <gustavomaescd@gmail.com>
2018-06-18 15:56:19 -0300

    Home / SemanticUI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import './css/app.css'
+import coverImg from './resources/cover.jpg'
+import logoImg from './resources/logo.png'
+import HomepageLayout from './HomepageLayout';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
+        <HomepageLayout />
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
